0433cf30f8f5d080e373159cda1aa4d06e5f439d
Vinayak <vinayakchhabra545.vc@gmail.com>
2025-06-24 12:54:49 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A small hands-on project to understand mlops concepts practically',
+    author='vinayak chhabra',
+    license='',
+)
